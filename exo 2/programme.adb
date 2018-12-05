0ccc31ac7ac59bfaa70c9bd86c8d10d8e63f07db
luca <lucachapelle@gmail.com>
2018-12-05 11:00:30 +0100

    a

--- exo 2/programme.adb
+++ exo 2/programme.adb
@@ -6,35 +6,96 @@ TYPE Noeud;
 TYPE ptNoeud IS ACCESS Noeud;
 TYPE Noeud IS
 RECORD
- Info : elt;
- Suiv : ptNoeud;
+ info : integer;
+ suiv : ptNoeud;
 END RECORD;
 
 TYPE File IS
 RECORD
- First : ptNoeud;
- Last  : ptNoeud;
+ first : ptNoeud;
+ last  : ptNoeud;
 END RECORD;
 
 File_Vide : CONSTANT File := (NULL, NULL);
 
+  FUNCTION Est_Vide(FileF: IN File) RETURN Boolean IS
+
+  BEGIN
+     RETURN FileF.First = NULL AND FileF.Last = NULL ;
+  END Est_Vide;
+
+PROCEDURE  afficheliste (maliste : IN file) is	
+	ptint : ptNoeud;
+begin
+        IF NOT Est_Vide(maliste) THEN
+		ptint := maliste.First;
+		Put("voila ma liste :");
+         WHILE NOT (ptint = NUll) LOOP
+            Put(Integer'Image(ptint.info));
+            ptint := ptint.Suiv;
+         END LOOP;
+	else 
+ 		put("ma liste est vide");
+	end if;	
+END afficheliste;
+  
+Function compte( lsite : ptnoeud
+PROCEDURE ajouttete(FileF: IN OUT File; Elmt: IN integer) IS
+      Pt_Int: ptNoeud;
+
+   BEGIN
+      IF Est_Vide(FileF) THEN
+         FileF.First := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.First;
+
+      ELSE
+         Pt_Int := FileF.First;
+         FileF.first := NEW Noeud'(Elmt,PT_Int);
+      END IF;
+
+    END ajouttete;
+
+ PROCEDURE ajoutfin(FileF: IN OUT File;
+                        Elmt : in integer)  IS
+   BEGIN
+      IF Est_Vide(FileF) THEN
+         FileF.First := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.First;
+
+      ELSE
+         FileF.Last.suiv := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.Last.Suiv;
+      END IF;
+
+   END ajoutfin;
+
    PROCEDURE Traiter (
          ChoixF   : IN     Integer;
-         maliste : IN OUT File) IS
+         maliste : IN OUT File;
+	fini : IN OUT boolean) IS
+  elem : integer;
+	res : integer;
+	compteur : integer := 0 ;
    BEGIN
       CASE ChoixF IS
          WHEN 1 =>
-		put(1);
-		--affiche(maliste);
+		afficheliste(maliste);
          WHEN 2 =>
-		put(1);
-		--compte(maliste);
+		res = compte(maliste.first,compteur);
+		Put("il y a " );Put(res);Put(" nombre(s) dans ma liste" )
+		New_Line
          WHEN 3 =>
-		put(1);
-		--ajouttete(maliste);
+		Put("veuillez entrer un nombre" );
+		New_Line;
+		Get(elem);
+      		New_Line;
+		ajouttete(maliste,elem);
          WHEN 4 =>
-		put(1);
-		--ajoutfin(maliste);
+		Put("veuillez entrer un nombre" );
+		New_Line;
+		Get(elem);
+      		New_Line;
+		ajoutfin(maliste,elem);
          WHEN 5 =>
 		put(1);
 		--ajoutrang(maliste);
@@ -57,7 +118,8 @@ choix : integer;
 fini : boolean := False;
 begin
    LOOP
-      Put ("Appuyer sur 1 pour lister la liste");
+      New_Line;
+      Put ("Appuyer sur 1 pour afficher la liste");
       New_Line;
       Put("Appuyer sur 2 pour compter le nombre d élements");
       New_Line;
@@ -75,7 +137,8 @@ begin
       New_Line;
       Get(Choix);
       New_Line;
-      Traiter (choix,maliste,fini);
+      Traiter (choix,maliste,fini);	
+      New_Line;
       EXIT WHEN fini;
    END LOOP;
 end exo2 ;
