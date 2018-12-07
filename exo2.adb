73f44a7397e891a0847699ce2738c84e3ecca84d
Kévin Keriche <36231816+KwikApu@users.noreply.github.com>
2018-12-07 22:50:20 +0100

    Add files via upload
    ordonner mise a jour du Last

--- exo2.adb
+++ exo2.adb
@@ -20,12 +20,19 @@ END RECORD;
 File_Vide : CONSTANT File := (NULL, NULL);
 
   FUNCTION Est_Vide(FileF: IN File) RETURN Boolean IS
+	-- Indique si la Liste est vide ou non.
+	      --IN
+		--FileF: La Liste qui est testée.
 
   BEGIN
      RETURN FileF.First = NULL AND FileF.Last = NULL ;
   END Est_Vide;
 
 PROCEDURE  afficheliste (maliste : IN file) is	
+	-- Cette procédure affiche tous les elements de la liste.
+      		-- IN
+      		   -- maliste: La Liste qui sera affiché.
+		  
 	ptint : ptNoeud;
 begin
         IF NOT Est_Vide(maliste) THEN
@@ -40,7 +47,11 @@ begin
 	end if;	
 END afficheliste;
   
-Function compte( liste : ptnoeud; cpt : integer) return integer is
+Function compte( liste : IN ptnoeud; cpt : IN integer) return integer is
+	-- Cette focntion compte le nombre d'element dans la liste.
+     		 -- IN
+      		   -- liste: La Liste contenant les elements à compter.
+      		   -- cpt: Le compteur de recursivité.
 Begin
 	if (liste = null) then 
 		return cpt;
@@ -50,6 +61,11 @@ Begin
 end compte;
 
 PROCEDURE ajouttete(FileF: IN OUT File; Elmt: IN integer) IS
+	-- Cette procédure ajoute un élément en debut de Liste.
+	      -- IN OUT
+	         -- FileF: La Liste où sera retiré l'élément.
+	      --IN
+		--Elmt : L'élément à retirer.
       Pt_Int: ptNoeud;
 
    BEGIN
@@ -64,8 +80,12 @@ PROCEDURE ajouttete(FileF: IN OUT File; Elmt: IN integer) IS
 
     END ajouttete;
 
- PROCEDURE ajoutfin(FileF: IN OUT File;
-                        Elmt : in integer)  IS
+ PROCEDURE ajoutfin(FileF: IN OUT File; Elmt : in integer)  IS
+	-- Cette procédure ajoute un element en fin de liste.
+	      -- IN OUT
+	         -- FileF: La Liste où l'element sera ajouté.
+	      -- IN
+		-- Elmt: L'élément à ajouter.
    BEGIN
       IF Est_Vide(FileF) THEN
          FileF.First := NEW Noeud'(Elmt,NULL);
@@ -79,6 +99,13 @@ PROCEDURE ajouttete(FileF: IN OUT File; Elmt: IN integer) IS
    END ajoutfin;
 
 PROCEDURE ajoutrang( mafile : IN OUT File; rang : IN Integer; Elt : IN Integer) IS
+	-- Cette procédure ajoute un element au rang donné.
+	      -- IN OUT
+	         -- mafile: La Liste où l'element sera ajouté.
+	      -- IN
+		-- Elt: L'élément à ajouter.
+		-- rang: Le rang où l'élément sera ajouté
+	
 	maliste : ptNoeud := mafile.first;
 	ptint : ptNoeud;
 Begin
@@ -94,6 +121,14 @@ Begin
 end ajoutrang;
 
 PROCEDURE supoccu( mafile : IN OUT File; Elt : IN Integer) IS
+	-- Cette procédure supprime les occurences d'un element dans la liste.
+      		-- IN OUT
+        	 	-- ma file : La Liste où l'element sera supprimé.
+      		-- IN
+			-- Elt: L'élément à supprimé.	
+
+
+--Déclarations et initialisations
 maliste : ptNoeud := mafile.first;
 ptprec : ptNoeud := mafile.first;
 trouve : boolean := False;
@@ -101,6 +136,7 @@ trouve : boolean := False;
 
 Begin
 
+	-- Gestion du premier élément
 	While (not Est_Vide(mafile)) and then maliste.info = elt loop
 		if compte(mafile.first,0) > 1 then
 			maliste := maliste.suiv;
@@ -109,7 +145,8 @@ Begin
 			mafile := File_Vide;
 		end if;
 	end loop;
-
+	
+	-- Suppression des occurences
 	if (not Est_Vide(mafile)) then		
 		ptprec := maliste;		
 		while not (maliste = NUll) loop			
@@ -123,16 +160,22 @@ Begin
 			end if;	
 			maliste := maliste.suiv;
 		end loop;
-		end if;
+	end if;
 
 	if not trouve then 
 		put ("Plus d'element dans la liste");
 	end if;
 end supoccu;
 
+
+
 PROCEDURE Ordonner(mafile : IN OUT File) IS
+	-- Cette procédure ordonne de manière croissante les elements de la liste.
+	      -- IN OUT
+	         -- mafile: La Liste des elements.
+	      
 	nouvF : File ;
-	nouvF_int : ptNoeud;
+	nouvF_int, pt_last, pt_before_last : ptNoeud;
 	ptint : ptNoeud := mafile.first;
 	cpt_loop : integer := 1 ;
 BEGIN
@@ -141,28 +184,46 @@ BEGIN
 		Put("Liste Vide");
 	ELSE
 		nouvF.first := new Noeud'(mafile.first.info, null);
+		nouvF.last := nouvF.first;
 		ptint := ptint.suiv;
+		
 	
 		FOR i IN 1..compte(ptint,0)-1 LOOP
+			
 			nouvF_int := nouvF.first;
+		
 			WHILE (nouvF_int /= null) AND THEN (ptint.info > nouvF_int.info) LOOP
 				cpt_loop := cpt_loop + 1;
+				pt_before_last := nouvF_int;
 				nouvF_int := nouvF_int.suiv;
-			END LOOP;				
-			ajoutrang(nouvF,cpt_loop,ptint.info);
-			ptint := ptint.suiv;
-			cpt_loop := 1 ;
+			END LOOP;			
+
+			IF (nouvF_int /= null) THEN	
+				ajoutrang(nouvF,cpt_loop,ptint.info);
+				ptint := ptint.suiv;
+				cpt_loop := 1 ;
+			ElSE
+				ajoutrang(nouvF,cpt_loop,ptint.info);			
+				pt_last := pt_before_last.suiv;
+				ptint := ptint.suiv;
+				cpt_loop := 1 ;
+			END IF;	
+						
+			
 		END LOOP;
 		mafile.first := nouvF.first;
-		mafile.last := nouvF.last;
+		mafile.last := pt_last;
 	END IF;
 END Ordonner;
 
-   PROCEDURE Traiter (
-         ChoixF   : IN     Integer;
-         maliste : IN OUT File;
-	fini : IN OUT boolean) IS
-  elem , rang: integer;
+   PROCEDURE Traiter (ChoixF : IN Integer; maliste : IN OUT File; fini : IN OUT boolean) IS
+	-- Cette procédure permet de traiter le choix dans le menu.
+      		-- IN OUT
+        		 -- maliste: La Liste à traité.
+		 	-- fini : booléen qui met fin au menu.
+      		-- IN
+			-- ChoixF: Le choix de l'utilisateur.
+ 	 elem , rang: integer;
 	res : integer;
    BEGIN
       CASE ChoixF IS
@@ -220,6 +281,7 @@ choix : integer;
 fini : boolean := False;
 begin
    LOOP
+	-- Le Menu
       New_Line;
       Put ("Appuyer sur 1 pour afficher la liste");
       New_Line;
