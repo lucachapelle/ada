97005c5c8a9b2d02191e44c5592c8436ff8e8907
Kévin Keriche <36231816+KwikApu@users.noreply.github.com>
2018-12-07 17:48:48 +0100

    Add files via upload
    Pour Ordonner il faut juste que je mette a jour le maliste.last et suppoccu pas touché
    la je me met au commentaire et redac. comme ca c'est fait!

--- programme.adb
+++ programme.adb
@@ -1,95 +1,235 @@
+
 with Ada.Text_IO, Ada.Integer_Text_IO ;
 use Ada.Text_IO, Ada.Integer_Text_IO ;
 
-procedure tp2 is
-   n:integer;
-   type noeud;
-   type ptliste is access noeud;
-   type noeud is record
-		info : integer;
-		suiv : ptliste;
-		end record;	
-  type Tab is array (0..9) of ptliste ;
-  liste_vide : CONSTANT ptliste :=NULL;	
+procedure exo2 is
+TYPE Noeud;
+TYPE ptNoeud IS ACCESS Noeud;
+TYPE Noeud IS
+RECORD
+ info : integer;
+ suiv : ptNoeud;
+END RECORD;
 
-  PROCEDURE  remplir_tab (montab : OUT Tab) is
+TYPE File IS
+RECORD
+ first : ptNoeud;
+ last  : ptNoeud;
+END RECORD;
+
+File_Vide : CONSTANT File := (NULL, NULL);
+
+  FUNCTION Est_Vide(FileF: IN File) RETURN Boolean IS
+
   BEGIN
-	FOR i IN 0..9 LOOP	
-		montab(i) := liste_vide ;
-	END LOOP;
-  END remplir_tab;	
+     RETURN FileF.First = NULL AND FileF.Last = NULL ;
+  END Est_Vide;
 
-  function  est_vide (maliste : in ptliste) return boolean is
-  BEGIN
-	return maliste = NULL;
-  END est_vide;
-
-  PROCEDURE Insere_tete(liste: IN OUT ptliste;
-                        n : in integer)  IS
-	ptint : ptliste;
-   BEGIN
-      IF est_vide(liste) THEN
-         liste := NEW noeud'(n,NULL);
-      ELSE
-	ptint := liste;
-	 liste := new noeud'(n,ptint);	
-	
-      END IF;
-   END Insere_tete;
-
-  PROCEDURE  traiter (montab : IN OUT Tab ; n : integer) is
-  indice : integer;
-  BEGIN
-	indice := n mod 10;
-	if est_vide(montab(indice)) then
-		montab(indice) := new noeud'(n ,liste_vide);
-	else
-		Insere_tete(montab(indice),n);	
-	end if;
-  END traiter;
-
- PROCEDURE  afficheliste (maliste : IN ptliste) is
-		ptint : ptliste;
-	begin
+PROCEDURE  afficheliste (maliste : IN file) is	
+	ptint : ptNoeud;
+begin
         IF NOT Est_Vide(maliste) THEN
-		ptint := maliste;
-         WHILE NOT Est_Vide(ptint) LOOP
+		ptint := maliste.First;
+		Put("voila ma liste :");
+         WHILE NOT (ptint = NUll) LOOP
             Put(Integer'Image(ptint.info));
             ptint := ptint.Suiv;
          END LOOP;
+	else 
+ 		put("ma liste est vide");
 	end if;	
-  END afficheliste;
-
-  PROCEDURE  affiche (montab : IN Tab) is
-  BEGIN
-	FOR i IN 0..9 LOOP	
-		if montab(i) = liste_vide then
-			put("---");put(Integer'Image(i));put("---");
-			New_line;
-		else
-			put("---");put(Integer'Image(i));put("---");
-			New_line;
-			afficheliste(montab(i));
-			New_line;
-		end if; 	
-	END LOOP;
-  END affiche;
-
-
-
-montab : Tab;
-fini : boolean := FALSE;		
-begin
-   remplir_tab(montab);
-   Put("Saisir un nombre : ") ;
-        Get(n) ;
-	fini := n = -1;
-   while not fini loop	
-	if n >= 0 then 
-        	traiter(montab,n);
+END afficheliste;
+  
+Function compte( liste : ptnoeud; cpt : integer) return integer is
+Begin
+	if (liste = null) then 
+		return cpt;
+	else 
+		return compte(liste.suiv,cpt+1);
 	end if;
-        Get(n) ;
-	fini := n = -1;
-   end loop ;
-	affiche(montab);
-end tp2 ;
+end compte;
+
+PROCEDURE ajouttete(FileF: IN OUT File; Elmt: IN integer) IS
+      Pt_Int: ptNoeud;
+
+   BEGIN
+      IF Est_Vide(FileF) THEN
+         FileF.First := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.First;
+
+      ELSE
+         Pt_Int := FileF.First;
+         FileF.first := NEW Noeud'(Elmt,PT_Int);
+      END IF;
+
+    END ajouttete;
+
+ PROCEDURE ajoutfin(FileF: IN OUT File;
+                        Elmt : in integer)  IS
+   BEGIN
+      IF Est_Vide(FileF) THEN
+         FileF.First := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.First;
+
+      ELSE
+         FileF.Last.suiv := NEW Noeud'(Elmt,NULL);
+         FileF.Last := FileF.Last.Suiv;
+      END IF;
+
+   END ajoutfin;
+
+PROCEDURE ajoutrang( mafile : IN OUT File; rang : IN Integer; Elt : IN Integer) IS
+	maliste : ptNoeud := mafile.first;
+	ptint : ptNoeud;
+Begin
+	if rang = 1 then
+		ajouttete(mafile,elt);
+	else 
+		FOR i IN 1..rang-2 LOOP	
+			maliste := maliste.suiv;
+		end loop;
+	ptint := maliste.suiv;
+	maliste.suiv := new Noeud'(elt,ptint);
+	end if;
+end ajoutrang;
+
+PROCEDURE supoccu( mafile : IN OUT File; Elt : IN Integer) IS
+maliste : ptNoeud := mafile.first;
+ptprec : ptNoeud := mafile.first;
+trouve : boolean := False;
+
+
+Begin
+		
+	while not (maliste = NUll) loop			
+		mafile.last :=maliste;	
+		if maliste.info = ELT   then
+			ptprec.suiv := maliste.suiv;
+			maliste := maliste.suiv;
+			trouve := true;
+		else
+			maliste := maliste.suiv;
+			ptprec := ptprec.suiv;
+		end if;
+
+	end loop;
+
+	if not trouve then 
+		put ("pas d element dans la liste");
+	end if;
+end supoccu;
+
+PROCEDURE Ordonner(mafile : IN OUT File) IS
+	nouvF : File ;
+	nouvF_int : ptNoeud;
+	ptint : ptNoeud := mafile.first;
+	cpt_loop : integer := 1 ;
+BEGIN
+	
+	IF Est_Vide(mafile) THEN
+		Put("Liste Vide");
+	ELSE
+		nouvF.first := new Noeud'(mafile.first.info, null);
+		ptint := ptint.suiv;
+	
+		FOR i IN 1..compte(ptint,0)-1 LOOP
+			nouvF_int := nouvF.first;
+			WHILE (nouvF_int /= null) AND THEN (ptint.info > nouvF_int.info) LOOP
+				cpt_loop := cpt_loop + 1;
+				nouvF_int := nouvF_int.suiv;
+			END LOOP;				
+			ajoutrang(nouvF,cpt_loop,ptint.info);
+			ptint := ptint.suiv;
+			cpt_loop := 1 ;
+		END LOOP;
+		mafile.first := nouvF.first;
+		mafile.last := nouvF.last;
+	END IF;
+END Ordonner;
+
+   PROCEDURE Traiter (
+         ChoixF   : IN     Integer;
+         maliste : IN OUT File;
+	fini : IN OUT boolean) IS
+  elem , rang: integer;
+	res : integer;
+   BEGIN
+      CASE ChoixF IS
+         WHEN 1 =>
+		afficheliste(maliste);
+         WHEN 2 =>
+		res := compte(maliste.first,0);
+		Put("il y a " );Put(Integer'Image(res));Put(" nombres dans ma liste" );
+		New_Line;
+         WHEN 3 =>
+		Put("veuillez entrer un nombre" );
+		New_Line;
+		Get(elem);
+      		New_Line;
+		ajouttete(maliste,elem);
+         WHEN 4 =>
+		Put("veuillez entrer un nombre" );
+		New_Line;
+		Get(elem);
+      		New_Line;
+		ajoutfin(maliste,elem);
+         WHEN 5 =>
+		Put("veuillez entrer un rang" );
+		New_Line;
+		Get(rang);
+      		New_Line;
+		if ((rang > compte(maliste.first,0) + 2) or (rang < 1)) then 
+			Put("rang invalide" );
+			New_Line;
+		else
+			Put("veuillez entrer un element" );
+			New_Line;
+			Get(elem);
+      			New_Line;
+			ajoutrang(maliste,rang,elem);
+		end if;
+         WHEN 6 =>
+		Put("veuillez entrer un nombre" );
+		New_Line;
+		Get(elem);
+      		New_Line;
+		supoccu(maliste,elem);
+         WHEN 7 =>
+		ordonner(maliste);
+         WHEN 8 =>
+            Fini := True;
+         WHEN OTHERS =>
+            Put("Erreur : Action non valide.");
+            New_Line;
+
+      END CASE;
+   END Traiter;
+maliste : File;
+choix : integer;
+fini : boolean := False;
+begin
+   LOOP
+      New_Line;
+      Put ("Appuyer sur 1 pour afficher la liste");
+      New_Line;
+      Put("Appuyer sur 2 pour compter le nombre d élements");
+      New_Line;
+      Put("Appuyer sur 3 pour ajouter en tete" );
+      New_Line;
+      Put ("Appuyer sur 4 pour ajouter en fin");
+      New_Line;
+      Put("Appuyer sur 5 pour ajouter au rang i");
+      New_Line;
+      Put("Appuyer sur 6 pour detruire les occurences" );
+      New_Line;
+      Put ("Appuyer sur 7 pour reordonner la liste");
+      New_Line;
+      Put("Appuyer sur 8 pour quitter");
+      New_Line;
+      Get(Choix);
+      New_Line;
+      Traiter (choix,maliste,fini);	
+      New_Line;
+      EXIT WHEN fini;
+   END LOOP;
+end exo2 ;
